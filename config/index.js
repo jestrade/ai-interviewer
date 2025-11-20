e99702d8a20d2c759f044a3d6c15c8cf50300b7e
Jesus Estrada <jesuestrada@gmail.com>
2025-11-19 23:54:19 -0500

    updates

--- config/index.js
+++ config/index.js
@@ -1,6 +1,11 @@
 import dotenv from "dotenv";
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const config = {
   httpServer: {
